0862efd731f30faf43857a6739f574f79fe85a2c
Vishal Jagwani <55453257+VishalJagwani@users.noreply.github.com>
2021-02-13 19:35:28 +0530

    Add files via upload

---
+++ dataTypes.py
@@ -0,0 +1,30 @@
+# Fundamental Data types
+# int
+# float
+# complex
+# bool
+# str
+# list
+# tuple
+# set
+# dict
+
+# Classes -> custom types
+# Specialized Data Types
+# None
+# ========================================================================
+
+# print(type(2+4))
+# print(type(2-4))
+# print(type(2*4))
+# print(type(2/4))
+
+# print(type(24.56))
+
+# print(2 ** 3)  # power of 2 to 3
+# print(9 // 4)  # Gives rounded answer
+# print(5 % 4)  # gives Remainder
+
+# Math Functions
+print(round(3.9))
+print(abs(-20))  # no -ve values; It gives absolute value
